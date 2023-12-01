dc5a609843890e9ddadb165e910f12bf190aa4e2
Diego <20001269@alumnos.utleon.edu.mx>
2023-11-30 22:36:37 -0600

    dos

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "http://localhost:3000"
+     origins "http://localhost:3000","newvsgames-production.up.railway.app"
 
      resource "*",
        headers: :any,
